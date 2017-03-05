446d3c7a3ccc02cce63cff59907c556092a9893d
Admin <Admin@DMITRIY->
2017-03-05 22:28:01 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class Program
+    {
+        public static void DoIt()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("*");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(DoIt));
+            t.Start();
+        }
+
+       
+    }
+}
